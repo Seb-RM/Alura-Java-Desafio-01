2e24291651d509c1cada84ec65eaded82ab533a9
Seb-RM <122194477+Seb-RM@users.noreply.github.com>
2024-09-16 18:39:54 -0300

    primer commit

---
+++ src/Desafio.java
@@ -0,0 +1,15 @@
+public class Desafio {
+
+    public static void main(String[] args) {
+        String nombre = "Cliente 01";
+        String tipoDeCuenta = "Corriente";
+        double saldo = 1599.99;
+
+        System.out.println("*************************************");
+        System.out.println("\nNombre del cliente: "+nombre);
+        System.out.println("El tipo de cuenta es: "+tipoDeCuenta);
+        System.out.println("Su saldo disposable es: "+saldo+"U$");
+        System.out.println("\n*************************************");
+
+    }
+}
